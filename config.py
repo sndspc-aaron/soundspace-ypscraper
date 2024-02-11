e1f480a9e7bb2fe2607ed75ca359685b4f984cd8
Aaron Hogan <110538021+sndspc-aaron@users.noreply.github.com>
2024-02-11 08:53:54 -0500

    Reduced chunks for stability

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Configuration for web scraping
 PAGE_LIMIT = 2
 DOMAIN = 'https://www.yellowpages.com'
-CONCURRENT_REQUESTS = 40
+CONCURRENT_REQUESTS = 20
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
 
 # File paths
